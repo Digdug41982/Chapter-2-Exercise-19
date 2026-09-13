d026b621d1be1ee72353bddb68dfa7fd1d9ebb0c
andre <andre@ANDREW_LAPTOP>
2026-09-13 15:05:54 -0400

    Add project files.

---
+++ Chapter 2 Exercise 19.cpp
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+using namespace std;
+int main()
+{ 
+	int NYCF;
+	int DENF;
+	int PHXF;
+
+	NYCF = 85;
+	DENF = 88;
+	PHXF = 106;
+	cout << (NYCF * 0.02) + NYCF << endl;
+	cout << (DENF * 0.02) + DENF << endl;
+	cout << (PHXF * 0.02) + PHXF << endl;
+}
+
+
